2dd72134e43637d7c8d01aa017f155d1679fde7f
YONGXI YANG <reganyang0415@gamil.com>
2020-04-20 16:09:13 +0800

    hdfs permission

--- pydoop_try.py
+++ pydoop_try.py
@@ -1,4 +1,7 @@
 import pydoop.hdfs as hdfs
 
 path = "/tmp/yongxi/tfoutput/mnist_model/xx.txt"
-hdfs.dump('hello, world', path)
+# hdfs.dump('hello, world', path)
+
+with hdfs.open(path, mode='w', user='profile') as f:
+    print("fuck you hdfs!!", file=f)
