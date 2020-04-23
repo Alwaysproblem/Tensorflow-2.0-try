3c91d487ad931d4b8af179b81e0aba573b0c2ad5
YONGXI YANG <reganyang0415@gamil.com>
2020-04-23 16:50:54 +0800

    hdfs on test cluster

--- submit_yarn.sh
+++ submit_yarn.sh
@@ -11,27 +11,28 @@ CONDAENV=tf2dis
 export HADOOP_HOME=/usr/hdp/2.5.6.0-40/hadoop
 export CLASSPATH=$(hadoop classpath --glob)
 # export LD_LIBRARY_PATH=${PATH}
-export LD_LIBRARY_PATH=$(hadoop classpath):${JAVA_HOME}/jre/lib/amd64/server:/home/sdev/yongxi/env/tfhdfs/lib
+# export LD_LIBRARY_PATH=${PATH}:${CLASSPATH}:${JAVA_HOME}/jre/lib/amd64/server:/home/sdev/yongxi/env/tfhdfs/lib
+export LD_LIBRARY_PATH=${PATH}:${CLASSPATH}
 export PYSPARK_PYTHON="./${CONDAENV}_zip/${CONDAENV}/bin/python"
 export QUEUE=adx
 export SPARK_HOME=/home/sdev/yongxi/spark-2.4.4-bin-hadoop2.7
 
 # set paths to libjvm.so, libhdfs.so, and libcuda*.so
-export LIB_HDFS=./${CONDAENV}_zip/env/tfhdfs/lib                                     # path to libhdfs.so, for TF acccess to HDFS
+export LIB_HDFS=/app/tflib/                                     # path to libhdfs.so, for TF acccess to HDFS
+# export LIB_HDFS=./${CONDAENV}_zip/env/tfhdfs/lib                                     # path to libhdfs.so, for TF acccess to HDFS
 # already upto hdfs:///user-profile/yongxi/spark/env/tfhdfs/lib
 # export LIB_JVM=hdfs:///user-profile/yongxi/spark/env/tfjvm                           # path to libjvm.so
 # # set paths to libjvm.so, libhdfs.so, and libcuda*.so
 # export LIB_HDFS=$HADOOP_PREFIX/lib/native/Linux-amd64-64
 # # already upto hdfs:///user-profile/yongxi/spark/env/tfjvm
-# export LIB_JVM=$JAVA_HOME/jre/lib/amd64/server
-export LIB_JVM=./${CONDAENV}_zip/env/tfjvm
+export LIB_JVM=$JAVA_HOME/jre/lib/amd64/server
+# export LIB_JVM=./${CONDAENV}_zip/env/tfjvm
 
 # on the cluster the path for lihdfs.so and libjvm.so
 # /usr/hdp/2.5.6.0-40/usr/lib/libhdfs.so
 # /usr/lib/ams-hbase/lib/hadoop-native/libhdfs.so
 
 export HADOOP_USER_NAME=profile
-# export CLASSPATH=$(hadoop classpath --glob)
 
 # jar Package on the air
 TFCONNECTOR=hdfs:///user-profile/yongxi/spark/env/jars/spark-tensorflow-connector_2.11-1.15.0.jar
@@ -43,7 +44,7 @@ SPARK_WORKER_INSTANCES=5
 EXECUTOR_MEMORY=2G
 
 # Train configuration
-EPOCHS=10
+EPOCHS=2
 
 # Input and output and not "hdfs://" pre-ffix
 # and must obtain the write permission all the way of the path.
@@ -67,7 +68,7 @@ ${SPARK_HOME}/bin/spark-submit \
                     --conf spark.dynamicAllocation.enabled=false \
                     --conf spark.yarn.maxAppAttempts=1 \
                     --conf spark.executorEnv.HADOOP_USER_NAME=${HADOOP_USER_NAME} \
-                    --conf spark.network.timeout=3600s \
+                    --conf spark.network.timeout=360s \
                     --conf "spark.yarn.appMasterEnv.PYSPARK_PYTHON=${PYSPARK_PYTHON}" \
                     --conf spark.executorEnv.CLASSPATH=${CLASSPATH} \
                     --conf spark.executorEnv.LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${LIB_JVM}:${LIB_HDFS} \
